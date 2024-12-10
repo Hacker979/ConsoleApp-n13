af24f19d24ab0aca4a9bf0dbc10a5bf9e30f6f33
Kab-35-16 <Kab-35-16@KAB-31-11->
2024-12-10 09:05:11 +0200

    рп

--- Program.cs
+++ Program.cs
@@ -19,41 +19,56 @@ namespace ConsoleApp_n13
             switch (a)
             {
                 case 1:
-                    Console.WriteLine("Monday");
+                    Console.WriteLine("Январь");
                     break;
                 case 2:
-                    Console.WriteLine("Tuesday");
+                    Console.WriteLine("Февраль");
                     break;
                 case 3:
-                    Console.WriteLine("Wednesday");
+                    Console.WriteLine("Март");
                     break;
                 case 4:
-                    Console.WriteLine("Thursday");
+                    Console.WriteLine("Апрель");
                     break;
                 case 5:
-                    Console.WriteLine("Friday");
+                    Console.WriteLine("Май");
                     break;
                 case 6:
-                    Console.WriteLine("Saturday");
+                    Console.WriteLine("Июнь");
                     break;
                 case 7:
-                    Console.WriteLine("Sunday");
+                    Console.WriteLine("Июль");
                     break;
+                case 8:
+                    Console.WriteLine("Август");
+                    break;
+                case 9:
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case 10:
+                    Console.WriteLine("Октябрь");
+                    break;
+                case 11:
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case 12:
+                    Console.WriteLine("Декабрь");
+                    break;
+
                 default:
-                    Console.WriteLine("не верное число недели");
+                    Console.WriteLine("не верное число месяца");
                     break;
             }
         }
         /// <summary>
-        /// Задача 1. Дано целое число n, указывающее номер дня недели от 1 до 7.
-        /// По указанному номеру n вывести название соответствующего дня недели.
+        /// Задача 2. По номеру месяца вывести его название.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
 
 
-            int a = Input("Введите число недели: ");
+            int a = Input("Введите число месяца : ");
             Week(a);
             Console.ReadLine();
         }
